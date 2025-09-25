c06df619f7a166fc21def3705d1715fcbb62762d
iko2000 <shengelia1800@gmail.com>
2025-09-25 14:46:45 +0200

    putting back my desired times

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -40,12 +40,12 @@ function scheduleJob(
   console.log(`[${name}] Scheduled "${cronExpr}" (TZ=${TZ})`);
 }
 
-// scheduleJob("runweatherreport", "0 8,14 * * *", async () => {
-//   await runWeatherEmailReport();
-// });
-scheduleJob("runweatherreport", "* * * * *", async () => {
+scheduleJob("runweatherreport", "0 8,14 * * *", async () => {
   await runWeatherEmailReport();
 });
+// scheduleJob("runweatherreport", "* * * * *", async () => {
+//   await runWeatherEmailReport();
+// });
 
 
 function handleShutdown(signal: NodeJS.Signals) {
